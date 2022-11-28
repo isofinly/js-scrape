45990e57c7d04d3cedc9cea9e4763097379e22a7
isofinly <isofinly@protonmail.com>
2022-11-28 11:42:03 +0300

    minor changes

--- list.py
+++ list.py
@@ -1,26 +1,32 @@
-adress = 'vyazma'
-f = open('temp.txt', 'w', encoding="utf8")
-f1 = open(f'{adress}-pastvu-title.txt', 'r', encoding="utf8").read().splitlines()
-f2 = open(f'{adress}-pastvu-data.txt', 'r', encoding="utf8").read().splitlines()
-f3 = open(f'{adress}-links-order.txt', 'r', encoding="utf8").read().splitlines()
-f4 = open(f'{adress}-pastvu-links.txt', 'r', encoding="utf8").read().splitlines()
-for i in range(len(f2)):
-    f2_list = f2[i].split(",")
-    f2_list.reverse()
-    f.write( '{' + '\n' +
-             '"id"' + ':' + ' ' + str(i) + ',' + '\n' + 
-             '"title"' + ':' + ' ' +'"' + str(f1[i]) + '"' + ',' + '\n' + 
-             '"link"' + ':' + ' ' + '"' + f3[i] + '"' + ',' + '\n' + 
-             '"image"' + ':' + ' ' + '"' +  f4[i] + '"' + ',' + '\n' + 
-             '"list"' + ':' + ' ' + str(f2_list).replace("'", '"') + '\n'
-             + '},' + '\n' 
-             )
-    # f1.write('"' + f3[i] + '"' + ' ' + ':' + ' ' + str(f2_list).replace("'", '"') + ',' + '\n')
-    # print(f2_list)
-f.close()    
+# adress = 'vyazma'
+def aboba(adress):
+    f = open('temp.txt', 'w', encoding="utf8")
+    f1 = open(f'{adress}-pastvu-title.txt', 'r', encoding="utf8").read().splitlines()
+    f2 = open(f'{adress}-pastvu-data.txt', 'r', encoding="utf8").read().splitlines()
+    f3 = open(f'{adress}-links-order.txt', 'r', encoding="utf8").read().splitlines()
+    f4 = open(f'{adress}-pastvu-links.txt', 'r', encoding="utf8").read().splitlines()
+    for i in range(len(f2)):
+        f2_list = f2[i].split(",")
+        f2_list.reverse()
+        f.write( '{' + '\n' +
+                '"id"' + ':' + ' ' + str(i) + ',' + '\n' + 
+                '"title"' + ':' + ' ' +'"' + str(f1[i]) + '"' + ',' + '\n' + 
+                '"link"' + ':' + ' ' + '"' + f3[i] + '"' + ',' + '\n' + 
+                '"image"' + ':' + ' ' + '"' +  f4[i] + '"' + ',' + '\n' + 
+                '"list"' + ':' + ' ' + str(f2_list).replace("'", '"') + '\n'
+                + '},' + '\n' 
+                )
+        # f1.write('"' + f3[i] + '"' + ' ' + ':' + ' ' + str(f2_list).replace("'", '"') + ',' + '\n')
+        # print(f2_list)
+    f.close()    
 
-f5 = open('temp.txt', 'r', encoding="utf8").read()
-f6 = open(f'{adress}.json', 'w', encoding="utf8")
+    f5 = open('temp.txt', 'r', encoding="utf8").read()
+    f6 = open(f'out.json', 'w', encoding="utf8")
 
-f6.write('[' + '\n' + f5[:-2] +  '\n' + ']')
-f6.close()
+    f6.write('[' + '\n' + f5[:-2] +  '\n' + ']')
+    f6.close()
+aboba('vyazma')
+aboba('kronv')
+aboba('grivcova')
+aboba('birzha')
+aboba('chajkovskogo')
